dacc34df87100957e013bce5403a2cd4a3b3a4a4
Muntasir Biojid <muntasirbiojid@Muntasirs-MacBook-Pro.local>
2019-04-01 14:56:36 -0700

    Updates

---
+++ precache-manifest.392c5574467b7715449a717cccc83d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d43fcecf4928b0c254b",
+    "url": "/Contact-Manager/static/js/runtime~main.e499c403.js"
+  },
+  {
+    "revision": "4a8ad62ebeec3fd1d548",
+    "url": "/Contact-Manager/static/js/main.6f95ccfb.chunk.js"
+  },
+  {
+    "revision": "c0989731d0d9b6355845",
+    "url": "/Contact-Manager/static/js/2.88b5c646.chunk.js"
+  },
+  {
+    "revision": "4a8ad62ebeec3fd1d548",
+    "url": "/Contact-Manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "c0989731d0d9b6355845",
+    "url": "/Contact-Manager/static/css/2.af139f08.chunk.css"
+  },
+  {
+    "revision": "274350a77e4807e5e87d5f6efa869e80",
+    "url": "/Contact-Manager/index.html"
+  }
+];
